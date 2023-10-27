d97c26915b91eca7bc680bbfd7ea33328f7a67f0
Jeremy Montera <wittkopp.jeremy@gmail.com>
2023-10-27 16:51:22 -0400

    OptsFile1.1 - tore apart old OptsFileReader class in lieu of new Reader class which inherits from IReader

--- src/OptsFile/reader.py
+++ src/OptsFile/reader.py
@@ -1,43 +1,9 @@
 import os
 import pathlib
-from typing import Any, Dict, List
+from typing import Any, Dict
+
+from .abc import IReader
 
 
-class OptsFileReader:
-    @staticmethod
-    def _validate(filepath: pathlib.Path) -> None:
-        """
-        This will make sure the file exists and has the appropriate extension.
-
-        Args:
-            filepath:
-                The path to the file to read from.
-        """
-
-        assert filepath.exists(), "***ERROR***:\tThe file doesn't exist!"
-
-    @staticmethod
-    def read_from_file(filename: str) -> Dict[str, Any]:
-        """
-        This will read from an `OptsFile` file type. It is responsible for parsing the
-        structure of the OptsFile and converting it to a dictionary.
-
-        Args:
-            filename:
-                The name of the file to read from.
-
-        Returns:
-            opts_dict:
-                The converted dictionary.
-        """
-
-        # Convert filename string to `pathlib.Path` object and validate.
-        filepath: pathlib.Path = pathlib.Path(os.path.abspath(filename))
-        OptsFileReader._validate(filepath)
-
-        # Read from the `.of` file and populate the options dictionary.
-        opts_dict: Dict[str, Any] = {}
-        with open(filepath, "r") as f:
-            for line in f.readlines():
-                if len(line) > 1:
-                    print(f"{line.rstrip()=}")
+class Reader(IReader):
+    ...
