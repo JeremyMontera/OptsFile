6c57150228f73d9d4ca891709eba91f956b09cdf
Jeremy Montera <wittkopp.jeremy@gmail.com>
2023-10-27 22:11:56 -0400

    OptsFile1.3 - Added unit tests

---
+++ test/test_reader.py
@@ -0,0 +1,82 @@
+import collections
+import os
+import pathlib
+import pytest
+import unittest.mock
+
+from OptsFile.abc import IReader
+from OptsFile.reader import Reader, ReaderError
+
+@pytest.fixture
+def new_reader():
+    reader: Reader = Reader("sample.of")
+    return reader
+
+@pytest.fixture
+def sample_optsfile_text():
+    return [
+        "Car:\n", "\tMake: Honda\n", "\tModel: Accord", "\n",
+        "Bike:\n", "\tType: Mountain\n",
+    ]
+
+@pytest.fixture
+def sample_text_format_output():
+    entry = collections.namedtuple("entry", ["depth", "content"])
+    return [
+        entry(0, ["Car:"]),
+        entry(1, ["Make:", "Honda"]),
+        entry(1, ["Model:", "Accord"]),
+        entry(0, [""]),
+        entry(0, ["Bike:"]),
+        entry(1, ["Type:", "Mountain"])
+    ]
+
+def test_reader_init_error_file_not_found():
+    with pytest.raises(AssertionError) as exc:
+        reader = Reader("durp.of")
+
+    filename: pathlib.Path = pathlib.Path(os.path.abspath("durp.of"))
+    assert exc.value.args[0] == f"***ERROR***:\t{filename} does not exist!"
+
+def test_reader_init(new_reader):
+    assert isinstance(new_reader, IReader)
+    assert hasattr(new_reader, "_filename")
+    assert isinstance(new_reader._filename, pathlib.Path)
+    assert new_reader._filename == pathlib.Path(os.path.abspath("sample.of"))
+
+@unittest.mock.patch.object(Reader, "_format_text")
+@unittest.mock.patch("builtins.open")
+def test_reader_read_from_file(mock_format, mock_open, new_reader):
+    entries = new_reader.read_from_file()
+    mock_format.assert_called_once()
+    mock_open.assert_called_once()
+
+@unittest.mock.patch.object(Reader, "_check_for_tabs")
+def test_reader_format_text(
+    mock_check, sample_optsfile_text, sample_text_format_output
+):
+    mock_check.return_value = "\t"
+    entries = Reader._format_text(sample_optsfile_text)
+    assert "depth" in entries[0]._fields
+    assert "content" in entries[0]._fields
+    assert len(entries) == len(sample_optsfile_text)
+    for entry1, entry2 in zip(entries, sample_text_format_output):
+        assert entry1 == entry2
+
+@pytest.mark.parametrize(
+    ("scenario", "content" , "result"),
+    [
+        ("tabs", "\t\tBlah", "\t"),
+        ("spaces", "        Blah", "    "),
+        ("error", "Blah", ""),
+    ]
+)
+def test_reader_check_for_tabs(scenario, content, result):
+    if scenario == "error":
+        with pytest.raises(ReaderError) as exc:
+            Reader._check_for_tabs(content)
+
+        assert exc.value.args[0] == "***ERROR***:\tThe current line seems to be malformed!"
+
+    else:
+        assert Reader._check_for_tabs(content) == result
